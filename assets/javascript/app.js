523fdf2f1d5a2eb409bbabae561778d7b1c952fc
Michael Fleshman <michaelfleshman90@gmail..com>
2018-08-11 10:02:10 -0400

    file structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/5.3.1/firebase.js"></script>
+    <script>
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyB1sV3YvbPdUlAkg6AL6O3SyIk_tQFUPNQ",
+        authDomain: "project-one-29a01.firebaseapp.com",
+        databaseURL: "https://project-one-29a01.firebaseio.com",
+        projectId: "project-one-29a01",
+        storageBucket: "",
+        messagingSenderId: "510205582320"
+    };
+    firebase.initializeApp(config);
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+$("button").on("click", function( {
+
+});
