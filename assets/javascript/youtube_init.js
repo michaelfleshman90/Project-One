d453e94a69e7ca187569db0cb790b0cac9f3e729
holland cookson <hollandcookson@wifi_guest-10-38-119-114.uncc.edu>
2018-08-15 20:58:07 -0400

    MERGE THIS ONE

--- assets/javascript/youtube_init.js
+++ assets/javascript/youtube_init.js
@@ -2,11 +2,13 @@ function start() {
 gapi.client.init({
     'apiKey': 'AIzaSyDhJXRi_rpiqcitHtraHZwlrXkGP8S5uA8',
   }).then(function() {
-      loadAPIClientInterfaces();
+      gapi.client.load('youtube','v3', function() {
+          handleAPILoaded();
+      })
   
   }, function(reason) {
     console.log('Error: ' + reason.result.error.message);
   });
 };
-// 1. Load the JavaScript client library.
+
 gapi.load('client', start);
