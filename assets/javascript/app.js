ca212a1680f7f07df2475f79b77c3c5398f99b5e
Michael Fleshman <michaelfleshman90@gmail..com>
2018-08-18 10:01:16 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,64 +7,79 @@ var config = {
     messagingSenderId: "510205582320"
 };
 firebase.initializeApp(config);
-
+function googleApiClientReady() {
+    gapi.client.setApiKey("AIzaSyCiLx7vHvqWnU76n3X0I03awrOaV3pbk4Q");
+    gapi.client.load("youtube", "v3", function() {
+        
+    });
+}
 function getTubey(event) {
-    var text = $(event.target).text();
-    var userSubmit = $("<input>");
-    userSubmit.attr("type", "text");
-    userSubmit.addClass("usertext");
-    var ytrailer = $(this).attr("data-movie");
-    var queryURL = "https://youtube.com/watch?v=" + ytrailer + "&key=AIzaSyCiLx7vHvqWnU76n3X0I03awrOaV3pbk4Q";
+    var text = $(event.target).val();
+    var queryURL = "https://www.googleapis.com/youtube/v3/videos?id=" + text + "&key=&fields=items(id,snippet(channelId,title,categoryId),statistics)&part=snippet,statistics";
     $.ajax({
         url: queryURL,
         method: "GET"
-    })
-        .then(function (response) {
-            console.log(response);
-            console.log(queryURL);
-            var results = response.data;
-
-            for (i = 0; i < results.length; i++) {
-                ytrailer = player.videoId;
-
-            }
-        });
-
-    $("#searchButton").on("click", function (event) {
-        document.preventDefault();
-        //loads the Iframe player API code asynchronously
-        var tag = document.createElement("script");
-        tag.src = "https://www.youtube.com/player_api";
-        var firstScriptTag = document.getElementsByTagName("script")[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        var player;
-
-        function onYouTubeIframeAPIReady() {
-            player = new YT.Player("youtubeDiv", {
-                height: "390",
-                width: "640",
-                videoId: "",
-                events: {
-                    "onReady": onPlayerReady,
-                    "onStateChange": onPlayerStateChange
-                }
-            });
-        };
-        function onPlayerReady(event) {
-            event.target.playVideo();
-        }
-        var done = false;
-        function onPlayerStateChange(event) {
-            if (event.data == YT.PlayerState.PLAYING && !done) {
-                done = true;
-            }
-        }
-        function stopVideo() {
-            player.stopVideo();
+    }).then(function (response) {
+        console.log(response);
+        console.log(queryURL);
+        var results = response.data;
+        console.log(results);
+        for (i = 0; i < results.length; i++) {
+            var listDiv = $("<div>");
+            listDiv.attr("src", results);
         }
     });
+    document.preventDefault();
+    //loads the Iframe player API code asynchronously
+    var tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/player_api";
+    var firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var player;
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player("youtubeDiv", {
+            height: "390",
+            width: "640",
+            videoId: "",
+            events: {
+                "onReady": onPlayerReady,
+                "onStateChange": onPlayerStateChange
+            }
+        });
+    };
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
+    var done = false;
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+            done = true;
+        }
+    }
+    function stopVideo() {
+        player.stopVideo();
+    }
 }
 function load() {
-    $(document.body).on("click", "#searchButton", getTubey);
-};
+    $("#searchButton").on("click", getTubey);
+    $('#myform').submit(function (event) {
+        event.preventDefault();
+    });
 
+    //var userSubmit = $("#userSearch").val();
+
+};
+function handleAPILoaded() {
+    $("#search-button").attr("disabled", false);
+}
+function search() {
+    var q = $("#searchButton").val();
+    var request = gapi.youtube.search.list({
+        q: q,
+        part: 'snippet'
+    });
+    request.execute(function (response) {
+        var str = JSON.stringify(response.result);
+        $("#myForm").html("<pre>" + str + "</pre>");
+    });
+}
